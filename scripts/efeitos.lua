beaed3d2f57ab7572014df7f40d8dcf8397b3735
felipesflisboa <felipesflisboa@users.noreply.github.com>
2014-06-16 22:13:50 -0300

    Movimentação das cartas adicionada
    Movimentação das cartas adicionada.

--- scripts/efeitos.lua
+++ scripts/efeitos.lua
@@ -1,12 +1,11 @@
 --Efeitos de transição
-function transFade (item, delay, tempo )
-	 item.alpha=0
-	 transition.to( item, {delay=delay, time=tempo, alpha=1, transition=easing.inOutQuad })
-end
-
 function transFade (item, delay, tempo, funcOnComplete )
 	 item.alpha=0
-	 transition.to( item, {delay=delay, time=tempo, alpha=1, transition=easing.inOutQuad, onComplete=funcOnComplete })
+	 if funcOnComplete then
+		transition.to( item, {delay=delay, time=tempo, alpha=1, transition=easing.inOutQuad, onComplete=funcOnComplete })
+	 else
+		transition.to( item, {delay=delay, time=tempo, alpha=1, transition=easing.inOutQuad })
+	 end
 end
 
 function criarAnimacao( x, y, rotation, carta )
